2a1fb43a334cfd7121f5707acdd6bd4f85cb501c
Shawn2001 <shawnlow2001@gmail.com>
2022-04-03 00:12:47 +0800

    auto auto let's go

--- scripts/auto.py
+++ scripts/auto.py
@@ -10,21 +10,18 @@ from sensor_msgs.msg import Range
 from std_srvs.srv import SetBool, SetBoolResponse
 
 # to set an default initial state
-L = 0.4
-R = 0.04
+start = False
 current_velocity = 0.0
 current_orientation = 0.0
-reference_velocity = 0.0
-reference_orientation = 0.0
 ROBOTNAME = "uavcar"
-start = False
+reference_orientation = 0
 
 def startAutonomous(data):
-    global reference_velocity, reference_orientation
+    global reference_orientation
     if not start:
         # Please start the robot
         rospy.loginfo("Please start the service")
-        rospy.sleep(1)
+        rospy.sleep(5)
         return 
 
     range = data.range 
@@ -32,15 +29,12 @@ def startAutonomous(data):
     min_range = data.min_range # 0.2
     threshold = 0.5 # TO BE CHANGE
     if range < 0.5:
-        reference_velocity = 0.1 # Bump on the wall
-        reference_orientation += math.pi/180
-    # elif range < 0.3: 
-    #     # STOP AND TURN LEF0.1
-    #     reference_orientation += 0.018
-    #     reference_velocity = 0.1
+        reference_orientation = reference_orientation +math.pi/180
+        speed_pub.publish(0.1)
+        rotate_pub.publish(reference_orientation)
     else:
         # GO STRAIGHT
-        reference_velocity = 0.2
+        speed_pub.publish(0.2)
     
 
 
@@ -53,20 +47,7 @@ def start_server(req):
         start = False
         return SetBoolResponse(False, "The robot has halted")
 
-def cmd_vel_callback(data):
-    #callback function to get the reference cmd_vel from another rosnode
-    global reference_velocity
-    reference_velocity = data.data
-
-
-def cmd_orientation_callback(data):
-    #callback function to get the reference orientation from another rosnode
-    global reference_orientation
-    #bind orientation to [-pi,pi]
-    reference_orientation = 2*math.atan(math.tan(0.5*data.data))
-
-
-def gazebomodelstate_callback(data):
+def current_state_callback(data):
     #callback function to get the current states (velocity, position and etc) of our robot
     global current_velocity, current_orientation, linear_velocity
     robots = data.name
@@ -90,55 +71,19 @@ def gazebomodelstate_callback(data):
     (roll, pitch, yaw) = euler_from_quaternion([x_o,y_o,z_o,w]) 
     current_orientation = yaw
 
-    # get error that need to be tuned  --> error between reference orientation & current_orientation...
-    error = reference_orientation - current_orientation
-
-    #error term correction 
-    if (abs(error) > math.pi):
-        if error<0:
-            error = error + 2*math.pi
-        else:
-            error = error - 2*math.pi
-
-    # print(error)
-    #get the desire angular_velocity to apply to our robot from controller
-    angular_velocity = p_controller(error)
-
-    # convert to left & right wheel speed
-    left_wheel_speed, right_wheel_speed = convert(reference_velocity, angular_velocity)
-
-    # To publish into L & R motor
-    left_wheel_pub.publish(left_wheel_speed)
-    right_wheel_pub.publish(-1*right_wheel_speed)
-
-
-def p_controller(error):
-    #a feedback controller which trying to converge the "error" to zero
-    gain = 0.6
-    return gain*error
-
-
-def convert(linear_velocity, angular_velocity):
-    #differential drive equation
-    left_wheel_pub = ((2*linear_velocity) - (angular_velocity*L)) / (2*R)
-    right_wheel_pub = ((2*linear_velocity) + (angular_velocity*L)) / (2*R)
-    return left_wheel_pub, right_wheel_pub
 
 if __name__ == '__main__':
     # name of this node(controller_system)
     rospy.init_node('controller_system', anonymous=True)
 
     s = rospy.Service('LetsGo', SetBool, start_server)
-    # subscribe to three nodes ( cmd_vel, cmd_orientation, gazebo_model_state )
-    rospy.Subscriber("/ultrasonic_feedback", Range, startAutonomous)
-   
-    # subscribe to three nodes ( cmd_vel, cmd_orientation, gazebo_model_state )
-    rospy.Subscriber("/cmd_vel", Float64, cmd_vel_callback)
-    rospy.Subscriber("/cmd_orientation", Float64, cmd_orientation_callback)
-    rospy.Subscriber("/gazebo/model_states", ModelStates,gazebomodelstate_callback)
 
-    # publish to two nodes ( left_wheel_speed, right_wheel_speed )
-    left_wheel_pub = rospy.Publisher('/uavcar/jointL_velocity_controller/command', Float64, queue_size=2)
-    right_wheel_pub = rospy.Publisher('/uavcar/jointR_velocity_controller/command', Float64, queue_size=2)
+    # subscribe to two nodes (  gazebo_model_state, ultrasonic_feedback )
+    rospy.Subscriber("/ultrasonic_feedback", Range, startAutonomous)
+    rospy.Subscriber("/gazebo/model_states", ModelStates,current_state_callback)
+
+    # publish to two nodes ( cmd_vel, cmd_orientation)
+    speed_pub = rospy.Publisher('/cmd_vel', Float64, queue_size=2)
+    rotate_pub = rospy.Publisher('/cmd_orientation', Float64, queue_size=2)
 
     rospy.spin()  # simply keeps python from exiting until this node is stopped
