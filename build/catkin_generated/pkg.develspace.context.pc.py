739148f92d15a938e54fe3bc85caa3515ffda1bc
yuuxii <yuuxii0110@gmail.com>
2022-04-05 13:25:50 +0800

    fix teleop

--- build/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;geometry_msgs;sensor_msgs;nav_msgs;std_srvs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "uavionics_workshop2"
-PROJECT_SPACE_DIR = "/home/venti-lap/catkin_ws/src/uavionics_workshop2/build/devel"
-PROJECT_VERSION = "1.0.0"
